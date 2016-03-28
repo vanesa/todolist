432fe615fb81aa42b55ec21499e5ade9e6b5ef77
Vanesa <vanesa@gmx.net>
2016-03-29 00:24:32 +0200

    add mux router

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
-package main
+package main // package main is required for a standalone executable.
 
 import (
-    "fmt"
+    "fmt" // Implements formatted I/O. 
     "html"
     "log"
     "net/http"
+
+    "github.com/gorilla/mux" // mux router from the Gorilla Web Toolkit.
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
+	// Here we register three routes mapping URL paths to handlers.
+    router := mux.NewRouter().StrictSlash(true) // StrictSlash defines the trailing slash behavior for new routes.
+    router.HandleFunc("/", Index)
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+
+func Index(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 
 }
