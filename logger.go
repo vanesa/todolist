1199fda4caa3948686ab9e75f7b8e2ee1becfda4
Vanesa <vanesa@gmx.net>
2016-03-29 14:49:05 +0200

    add logger

---
+++ logger.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "time"
+)
+// Pass handler to this function, which will then wrap the passed handler with logging and timing functionality.
+func Logger(inner http.Handler, name string) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+
+        inner.ServeHTTP(w, r)
+
+        log.Printf(
+            "%s\t%s\t%s\t%s",
+            r.Method,
+            r.RequestURI,
+            name,
+            time.Since(start),
+        )
+    })
+}
