b37ac7fda94fb54fffc6d50c42d6a37a4dcc1514
Vanesa <vanesa@gmx.net>
2016-03-29 11:59:35 +0200

    add routes

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main // package main is required for a standalone executable.
 
 import (
-    "fmt" // Implements formatted I/O. 
-    "html"
-    "log"
-    "net/http"
+    	"fmt" // Implements formatted I/O. 
+    	"html"
+    	"log"
+    	"net/http"
 
-    "github.com/gorilla/mux" // mux router from the Gorilla Web Toolkit.
+    	"github.com/gorilla/mux" // mux router from the Gorilla Web Toolkit.
 )
 
 func main() {
 	// Here we register three routes mapping URL paths to handlers.
-    router := mux.NewRouter().StrictSlash(true) // StrictSlash defines the trailing slash behavior for new routes.
-    router.HandleFunc("/", Index)
+	router := mux.NewRouter().StrictSlash(true) // StrictSlash defines the trailing slash behavior for new routes.
+    	router.HandleFunc("/", Index)
+    	router.HandleFunc("/todos", TodoIndex)
+    	router.HandleFunc("/todos/{todoId}", TodoShow)
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+    	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 
 func Index(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+        fmt.Fprintln(w, "Hello, %q", html.EscapeString(r.URL.Path))
 
+}
+
+func TodoIndex(w. http.ReponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+func TodoShow(w. http.ReponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, )
 }
