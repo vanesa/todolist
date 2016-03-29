e76c1d01e137ac38739701986fa5d0a60b70d718
Vanesa <vanesa@gmx.net>
2016-03-29 14:10:58 +0200

    refactor code, split handlers and routes from main

--- main.go
+++ main.go
@@ -1,45 +1,13 @@
 package main // package main is required for a standalone executable.
 
 import (
-    	"fmt" // Implements formatted I/O. 
-    	"html"
     	"log"
     	"net/http"
-	"encoding/json"
-
-    	"github.com/gorilla/mux" // mux router from the Gorilla Web Toolkit.
 )
 
 func main() {
 	// Here we register three routes mapping URL paths to handlers.
-	router := mux.NewRouter().StrictSlash(true) // StrictSlash defines the trailing slash behavior for new routes.
-    	router.HandleFunc("/", Index)
-    	router.HandleFunc("/todos", TodoIndex)
-    	router.HandleFunc("/todos/{todoId}", TodoShow)
-
+	router := NewRouter()
+	
     	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-
-func Index(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello, %q", html.EscapeString(r.URL.Path))
-
-}
-
-// Simulate a real response and mock out the TodoIndex with static data
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
-
-	if err := json.NewEncoder(w).Encode(todos); err != nil {
-		panic(err)
-	}
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo Show: ", todoId)
 }
